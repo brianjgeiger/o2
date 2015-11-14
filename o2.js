58b9ba4f9b593e16bdc0155bc2364b0eff06df93
Brian J. Geiger <bgeiger@pobox.com>
2015-11-14 17:26:13 -0500

    Create bidirectional communication and make id fancier

--- o2.js
+++ o2.js
@@ -2,8 +2,11 @@ var menuBar = require('menubar');
 var Client = require('node-rest-client').Client;
 var shell = require('shell');
 var ipc = require('ipc');
+var app = require('app');
 
-var mb = menuBar();
+var mbOptions = {"width": 1000, "height": 800};
+
+var mb = menuBar(mbOptions);
 var client = new Client();
 var webContents = null;
 client.registerMethod("nodes", "https://staging-api.osf.io/v2/nodes/", "GET");
@@ -11,27 +14,29 @@ client.registerMethod("my_nodes", "https://staging-api.osf.io/v2/users/me/nodes/
 
 var showNodes = function(nodes){
     "use strict";
-    console.log("Trying to send nodes");
-    console.log(nodes);
-    webContents.send('ping', 'whoooooooh!');
-    webContents.send('getNodes', nodes);
+    mb.window.send('getNodes', nodes);
 };
 
 mb.on('ready', function ready () {
     "use strict";
-    console.log('app is ready and updating');
-
-});
-
-mb.on('after-create-window', function ready () {
-    "use strict";
-    console.log("Created window");
-    webContents = mb.window.webContents;
-    webContents.on('did-finish-load', function() {
-        console.log("Content finished loading");
+    ipc.on('did-finish-load',function(){
         client.methods.nodes(function(data, response) {
             var json = JSON.parse(data.toString());
             showNodes(json.data);
         });
-     });
+    });
+
+    ipc.on('exit', function() {
+        app.quit();
+    });
+    console.log('app is ready and updating');
+});
+
+mb.on('after-create-window', function ready () {
+    "use strict";
+    webContents = mb.window.webContents;
+    mb.window.openDevTools();
+    //webContents.on('did-finish-load', function() {
+    //    console.log("Content finished loading");
+    // });
 });
